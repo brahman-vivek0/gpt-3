7d73762849d7e70232264765bdcfba56dd9597c1
Vivek Tiwari <163820334+brahman-vivek0@users.noreply.github.com>
2025-06-16 13:21:26 +0530

    script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = "AIzaSyAu1kYC2dGh8kYcGadKUumyetDkb5kFLs8"; // Your API key here
-const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
+const API_KEY = "AIzaSyAvIvUFfIvp1EKmSBVspNHo8wpXMxbChMI"; // Your API key here
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
 const loadDataFromLocalstorage = () => {
